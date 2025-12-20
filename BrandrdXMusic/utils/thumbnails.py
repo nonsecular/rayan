a1065e7cde67887156e3a1f0e644c2e1b042b4d8
Nobi-123 <nobita.infringement.report@gmail.com>
2025-12-20 22:04:27 +0530

    Replace youtubesearchpython with py_yt for video search

--- BrandrdXMusic/utils/thumbnails.py
+++ BrandrdXMusic/utils/thumbnails.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageDraw, ImageEnhance
 from PIL import ImageFilter, ImageFont, ImageOps
 
 from unidecode import unidecode
-from youtubesearchpython.__future__ import VideosSearch
+from py_yt import VideosSearch
 
 from BrandrdXMusic import app
 from config import YOUTUBE_IMG_URL
