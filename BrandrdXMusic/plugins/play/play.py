c02ca90bc0e31c22da43762335fc45da182839f8
Nobi-123 <nobita.infringement.report@gmail.com>
2025-12-20 22:03:12 +0530

    Update play.py

--- BrandrdXMusic/plugins/play/play.py
+++ BrandrdXMusic/plugins/play/play.py
@@ -54,7 +54,7 @@ async def play_commnd(
     fplay,
 ):
     mystic = await message.reply_text(
-        _["play_2"].format(channel) if channel else _["play_1"]
+        _["play_2"].format(channel) if channel else random.choice(AYU)
     )
     plist_id = None
     slider = None
